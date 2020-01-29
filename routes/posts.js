363034edc2aa54119e00bddcee168f9485ec2b0a
franciscoescobar <franciscoescobar@users.noreply.github.com>
2020-01-29 18:46:15 -0300

    uploaded keys

--- routes/posts.js
+++ routes/posts.js
@@ -4,9 +4,9 @@ const awsUpload = require("../utils/aws-upload");
 
 const router = express.Router();
 
-router.get('/posts', postController.getPosts);
-router.post('/post', awsUpload, postController.createPost);
-router.patch('/post/:postId', postController.editPost);
-router.delete('/post/:postId', postController.deletePost);
+router.get("/posts", postController.getPosts);
+router.post("/post", awsUpload, postController.createPost);
+router.patch("/post/:postId", postController.editPost);
+router.delete("/post/:postId", postController.deletePost);
 
 module.exports = router;
--- utils/aws-upload.js
+++ utils/aws-upload.js
@@ -3,36 +3,31 @@ const multer = require("multer");
 const multerS3 = require("multer-s3");
 
 aws.config.update({
-    secretAccessKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-    accessKeyId: 'XXXXXXXXXXXXXXX',
-    region: 'us-east-1'
+  secretAccessKey: "NCGPXTsAlkUT90ccFXPVGzjFFE7Vkx8L4A1NFmiI",
+  accessKeyId: "AKIAZETXKKH3ZBTGVMOJ",
+  region: "us-east-1"
 });
 
 const s3 = new aws.S3({});
 
 const fileFilter = (req, file, cb) => {
-    if (
-        file === "image/png" ||
-        file === "image/jpg" ||
-        file === "image/jpeg" 
-    ) {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-}
+  if (file === "image/png" || file === "image/jpg" || file === "image/jpeg") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 const upload = multer({
-    storage: multerS3({
-        s3,
-        bucket: "ghm-gallery",
-        metadata: (req,file,cb) => {
-            cb(null, {fieldName: file.fieldname});
-        },
-        key:(req, file, cb) => {
-            cb(null, Date.now().toString());
-        }
-    }),
-    fileFilter
-}).single('image')
+  storage: multerS3({
+    s3,
+    bucket: "ghm-gallery",
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: (req, file, cb) => {
+      cb(null, Date.now().toString());
+    }
+  })
+}).single("image");
 
 module.exports = upload;
